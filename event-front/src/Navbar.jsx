2645c127d1036f9f98b667d602b5e66d3aed93b6
El Abdou Drame <eha.d1@zig.univ.sn>
2023-11-29 02:25:34 +0000

    Modification des navbars et footers

--- event-front/src/Navbar.jsx
+++ event-front/src/Navbar.jsx
@@ -1,47 +1,134 @@
-import { Link, NavLink, Outlet } from "react-router-dom";
+import { Avatar, Menu, MenuItem } from '@mui/material';
+import React from 'react';
+import { Link, NavLink, Outlet } from 'react-router-dom';
 import Logo from './assets/Logo.png';
 import './styles/Navbar.css';
 
-/**
- * Composant Navbar qui represente  l'en-tete de nos page web
- * @returns {HTMLElement}
- */
+function Navbar() {
+    const token = sessionStorage.getItem('jwt');
+    const [anchorEl, setAnchorEl] = React.useState(null);
 
-function Navbar (){
-    return(
+    const handleMenuClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleMenuClose = () => {
+        setAnchorEl(null);
+    };
+
+    const handleMyAccountClick = () => {
+        window.location.href = './pages/Profile';
+        console.log("Mon compte");
+        handleMenuClose();
+    };
+
+    const handleMyEventsClick = () => {
+        // Ajoutez le code pour gérer le clic sur "Mes événements"
+        // Par exemple, vous pouvez rediriger l'utilisateur vers la page de ses événements.
+        console.log("Mes événements");
+        handleMenuClose();
+    };
+
+    const handleLogoutClick = () => {
+        sessionStorage.removeItem('jwt');
+        window.location.reload();
+        console.log("Se déconnecter");
+        handleMenuClose();
+    };
+
+    return (
         <>
             <header id="navbar">
                 <div id="navbar-content">
-                    <nav id="nav-item">
                     <Link to="./pages/Home" className="logo-link">
-                        <img src={Logo} alt="Le logo de notre site" style={{ width: '75px', height: '75px'}}/>
+                        <img src={Logo} alt="Le logo de notre site" style={{ width: '75px', height: '75px' }} />
+                        <h1>Sen Khew</h1>
                     </Link>
-                        
-                        <div id="links">
-                            <ul>
-                                <li>
-                                    <NavLink to='./pages/Home'>Accueil</NavLink>
-                                </li>
-                                <li>
-                                    <NavLink to='./pages/Services'>Services</NavLink>
-                                </li>
-                                <li>
-                                    <NavLink to='./pages/Galleries'>Galleries</NavLink>
-                                </li>
-                                <li>
-                                    <NavLink to='./pages/CreateEvent'>Create Event</NavLink>
-                                </li>
-                            </ul>
-                        </div>
-                    </nav>
+
+                    <div id="links">
+                        <ul>
+                            <li>
+                                <NavLink to='./pages/Home'>Accueil</NavLink>
+                            </li>
+                            <li>
+                                <NavLink to='./pages/Services'>Services</NavLink>
+                            </li>
+                            <li>
+                                <NavLink to='./pages/Galleries'>Galleries</NavLink>
+                            </li>
+                            <li>
+                                <NavLink to='./pages/CreateEvent'>Create Event</NavLink>
+                            </li>
+                        </ul>
+                    </div>
+
                     <div id="connect-buttons">
-                        <NavLink to='./pages/Login'>Se Connecter</NavLink>
-                        <NavLink to='./pages/Signup'>S'inscrire</NavLink>
+                        {token ? (
+                            // Si un jeton est présent, affichez l'avatar et le menu déroulant
+                            <>
+                                <Avatar
+                                    alt="User Avatar"
+                                    src="lien_de_l_avatar.jpg" // Remplacez par le lien de l'avatar
+                                    sx={{ cursor: 'pointer' }}
+                                    onClick={handleMenuClick}
+                                />
+                                <Menu
+                                    id="menu-appbar"
+                                    anchorEl={anchorEl}
+                                    anchorOrigin={{
+                                        vertical: 'top',
+                                        horizontal: 'right',
+                                    }}
+                                    transformOrigin={{
+                                        vertical: 'top',
+                                        horizontal: 'right',
+                                    }}
+                                    style={{
+                                        marginTop: '50px',
+                                    }}
+                                    keepMounted
+                                    open={Boolean(anchorEl)}
+                                    onClose={handleMenuClose}
+                                >
+                                    <MenuItem
+                                        onClick={handleMyAccountClick}
+                                        style={{
+                                            fontFamily: 'Libre Baskerville',
+                                        }}
+                                    >
+                                        Mon compte
+                                    </MenuItem>
+                                    <MenuItem
+                                        onClick={handleMyEventsClick}
+                                        style={{
+                                            fontFamily: 'Libre Baskerville',
+                                        }}
+                                    >
+                                        Mes événements
+                                    </MenuItem>
+                                    <MenuItem
+                                        onClick={handleLogoutClick}
+                                        style={{
+                                            fontFamily: 'Libre Baskerville',
+                                        }}
+                                    >
+                                        Se déconnecter
+                                    </MenuItem>
+                                </Menu>
+                            </>
+                        ) : (
+                            // Sinon, affichez les boutons de connexion
+                            <>
+                                <NavLink to='./pages/Login'>Se Connecter</NavLink>
+                                <NavLink to='./pages/Signup'>S'inscrire</NavLink>
+                            </>
+                        )}
                     </div>
                 </div>
             </header>
-            <Outlet/>
+            <Outlet />
         </>
-    )
+    );
 }
+
 export default Navbar;
