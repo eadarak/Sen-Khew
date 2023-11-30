473ac5fd114846b0b6825c29b2c5748e62e9d1fe
mirfou111 <fourmitux111@gmail.com>
2023-11-30 21:21:31 +0000

    Derniere modification page de visionnage des evenements

--- event-front/src/App.jsx
+++ event-front/src/App.jsx
@@ -3,7 +3,6 @@ import React from "react";
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import Navbar from "./Navbar";
 import CreateEvent from "./pages/CreateEvent";
-import DashClient from "./pages/DashClient";
 import Galleries from "./pages/Galleries";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
